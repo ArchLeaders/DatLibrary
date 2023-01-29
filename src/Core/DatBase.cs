680727c833f6d3bafd54b51f0759ee126d8307c7
ArchLeaders <archparliament@gmail.com>
2023-01-29 00:40:15 -0800

    Optimize parser (a lot) using simplified span parsing

--- src/Core/DatBase.cs
+++ src/Core/DatBase.cs
@@ -1,79 +1,110 @@
 ﻿using CeadLibrary.IO;
+using DatLibrary.Extensions;
 using System.Buffers;
+using System.Buffers.Binary;
 using System.Text;
 
 namespace DatLibrary.Core;
 
 public class DatBase
 {
-    public DatBase(CeadReader reader)
+    public DatBase(Stream stream)
     {
-        long hdrOffset = reader.ReadUInt32();
+        Span<byte> buffer32 = stackalloc byte[4];
+        stream.Read(buffer32);
+        long tableOffset = buffer32.ToUInt32();
 
-        if ((hdrOffset & 0x80000000) != 0) {
-            hdrOffset ^= 0xFFFFFFFF;
-            hdrOffset <<= 0x8;
-            hdrOffset += 0x100;
+        if ((tableOffset & 0x80000000) != 0) {
+            tableOffset ^= 0xFFFFFFFF;
+            tableOffset <<= 0x8;
+            tableOffset += 0x100;
         }
 
-        if (reader.BaseStream.Length > uint.MaxValue) {
-            hdrOffset = 0x0100000000 ^ hdrOffset;
+        if (stream.Length > uint.MaxValue) {
+            tableOffset = 0x0100000000 ^ tableOffset;
         }
 
-        using (reader.TemporarySeek(hdrOffset, SeekOrigin.Begin)) {
-            int archiveId = reader.ReadInt32();
-            uint fileCount = reader.ReadUInt32();
+        stream.Seek(tableOffset, SeekOrigin.Begin);
+        
+        stream.Read(buffer32);
+        int archiveId = BinaryPrimitives.ReadInt32LittleEndian(buffer32);
 
-            // Isn't the file count on newer archives?
-            if (fileCount != 0x3443432E && fileCount != 0x2e434334 && fileCount != 0x3443432E && fileCount != 0x2E434334) {
-                LoadLegacyHdr(reader, archiveId, fileCount);
-            }
-            else {
-                LoadHdr(reader);
-            }
+        stream.Read(buffer32);
+        int fileCount = BinaryPrimitives.ReadInt32LittleEndian(buffer32);
+
+        // Isn't the file count on newer archives?
+        if (fileCount != 0x3443432E && fileCount != 0x2e434334 && fileCount != 0x3443432E && fileCount != 0x2E434334) {
+            LoadLegacyHdr(stream, archiveId, fileCount);
+        }
+        else {
+            LoadHdr(null!);
         }
     }
 
-    public void LoadHdr(CeadReader reader)
+    public void LoadHdr(Stream stream)
     {
 
     }
 
-    public static void LoadLegacyHdr(CeadReader reader, int archiveId, uint fileCount)
+    public static void LoadLegacyHdr(Stream stream, int archiveId, int fileCount)
     {
-        var entries = new (uint offset, uint zsize, uint size, uint packed)[fileCount];
+        // Only allocate buffers larger than 1mb
+        int entryBufferSize = fileCount * 16;
+        Span<byte> entriesBufffer = entryBufferSize < 0x100000 ? stackalloc byte[entryBufferSize] : new byte[entryBufferSize];
+        stream.Read(entriesBufffer);
+
+        Span<byte> buffer = stackalloc byte[16];
+        var entries = new (string path, uint offset, uint zsize, uint size, uint packed)[fileCount];
         for (int i = 0; i < fileCount; i++) {
-            uint offset = reader.ReadUInt32();
+            buffer = entriesBufffer[(i * 16)..(i * 16 + 16)];
+
+            uint offset = buffer[..4].ToUInt32();
             if (archiveId != -1) {
                 offset <<= 0x8;
             }
 
-            uint zSize = reader.ReadUInt32();
-            uint size = reader.ReadUInt32();
-            uint packed = reader.ReadUInt32() & 0x00FFFFFF;
+            uint zSize = entriesBufffer[4..8].ToUInt32();
+            uint size = entriesBufffer[12..16].ToUInt32();
+            uint packed = entriesBufffer[16..].ToUInt32() & 0x00FFFFFF;
 
-            entries[i] = (offset + (packed >> 24), zSize, size, packed);
+            entries[i] = (null!, offset + (packed >> 24), zSize, size, packed);
         }
 
-        int nameCount = reader.ReadInt32();
-        long namesOffset = reader.BaseStream.Position + nameCount * (archiveId <= -5 ? 12 : 8) + 4; // not sure what the last DWord before the strings is
+        // Get string info entry count
+        Span<byte> buffer32 = stackalloc byte[4];
+        stream.Read(buffer32);
+        int nameCount = buffer32.ToInt32();
+
+        // Only allocate buffers larger than 1mb
+        int nodeSize = archiveId <= -5 ? 12 : 8;
+        int strInfoBufferSize = nameCount * nodeSize;
+        Span<byte> strInfoBuffer = strInfoBufferSize < 0x100000 ? stackalloc byte[strInfoBufferSize] : new byte[strInfoBufferSize];
+        stream.Read(strInfoBuffer);
+
+        // not sure what the last DWord before the strings is
+        long namesOffset = stream.Position + strInfoBufferSize + 4;
         string[] prevNames = ArrayPool<string>.Shared.Rent(nameCount);
-        string[] names = new string[fileCount];
+
+        Stack<string> root = new();
+        buffer = stackalloc byte[8];
 
         int index = 0;
         for (int i = 0; i < fileCount; i++) {
-            int next = 1;
             StringBuilder sb = new();
+
+            int next = 1;
             while (next > 0) {
+                buffer = strInfoBuffer[(index * 12)..(index * 12 + 12)];
+
                 // >0 means there's another node, but I'm
                 // assuming since the value isn't 1/0,
                 // that it contains other data
-                next = reader.ReadInt16();
-                
+                next = buffer[..2].ToInt16();
+
                 // Prepend the last path (probably poorly),
                 // clear may be redundant here, I don't
                 // think this is ever reached while(sb.Length > 0)
-                int prev = reader.ReadInt16();
+                int prev = buffer[2..4].ToInt16();
                 if (prev != 0) {
                     sb.Clear();
                     sb.Append(prevNames[prev]);
@@ -88,27 +119,19 @@ public class DatBase
 
                 // Read the string from an offset relative
                 // to the start of the string table
-                int offset = reader.ReadInt32();
+                int offset = buffer[4..8].ToInt32();
                 if (offset >= 0) {
-                    using (reader.TemporarySeek(namesOffset + offset, SeekOrigin.Begin)) {
-                        sb.Append(reader.ReadString(StringType.ZeroTerminated));
-                    }
+                    sb.Append("__entry__");
 
                     if (next > 0) {
                         sb.Append(Path.DirectorySeparatorChar);
                     }
                 }
-                
-                if (archiveId <= -5) {
-                    reader.Seek(4, SeekOrigin.Current);
-                }
 
                 index++;
             }
 
-            names[i] = sb.ToString();
+            entries[i].path = sb.ToString();
         }
-
-        ArrayPool<string>.Shared.Return(prevNames);
     }
 }
---
+++ src/Extensions/SpanExtension.cs
@@ -0,0 +1,46 @@
+﻿using System.Buffers.Binary;
+
+namespace DatLibrary.Extensions;
+
+public static class SpanExtension
+{
+    public static short ToInt16(this Span<byte> data, bool bigEndian = false)
+    {
+        if (bigEndian) {
+            return BinaryPrimitives.ReadInt16BigEndian(data);
+        }
+        else {
+            return BinaryPrimitives.ReadInt16LittleEndian(data);
+        }
+    }
+
+    public static ushort ToUInt16(this Span<byte> data, bool bigEndian = false)
+    {
+        if (bigEndian) {
+            return BinaryPrimitives.ReadUInt16BigEndian(data);
+        }
+        else {
+            return BinaryPrimitives.ReadUInt16LittleEndian(data);
+        }
+    }
+
+    public static int ToInt32(this Span<byte> data, bool bigEndian = false)
+    {
+        if (bigEndian) {
+            return BinaryPrimitives.ReadInt32BigEndian(data);
+        }
+        else {
+            return BinaryPrimitives.ReadInt32LittleEndian(data);
+        }
+    }
+
+    public static uint ToUInt32(this Span<byte> data, bool bigEndian = false)
+    {
+        if (bigEndian) {
+            return BinaryPrimitives.ReadUInt32BigEndian(data);
+        }
+        else {
+            return BinaryPrimitives.ReadUInt32LittleEndian(data);
+        }
+    }
+}
