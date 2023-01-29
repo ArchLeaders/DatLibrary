7373e5e7643e54dc83d640f059495df7e755a5f7
ArchLeaders <archparliament@gmail.com>
2023-01-29 00:41:05 -0800

    Create StringEntry struct

---
+++ src/Structs/StringEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DatLibrary.Structs;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x0C, Pack = 1)]
+public struct StringEntry
+{
+    [FieldOffset(0x00)]
+    public short BlobIndex;
+
+    [FieldOffset(0x02)]
+    public short NodeIndex;
+
+    [FieldOffset(0x04)]
+    public uint Offset;
+
+    [FieldOffset(0x08)]
+    public uint Unknown;
+}
