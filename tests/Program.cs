27cae390a067f9ceac2f5caf6ef208fb432623b4
ArchLeaders <archparliament@gmail.com>
2023-01-28 19:26:07 -0800

    Create debug runner

---
+++ tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using CeadLibrary.IO;
+using DatLibrary.Core;
+using System.Diagnostics;
+
+Stopwatch watch = Stopwatch.StartNew();
+
+FileStream fs = File.OpenRead("D:\\Games\\LegoDimensions\\Game\\content\\GAME.DAT");
+CeadReader reader = new(fs);
+DatBase _base = new(reader);
+
+watch.Stop();
+
+Console.WriteLine($"Elapsed Milliseconds: {watch.ElapsedMilliseconds}");
