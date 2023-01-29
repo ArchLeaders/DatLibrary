d3eaaeeaf7229de283481cc815b677141db349c8
ArchLeaders <archparliament@gmail.com>
2023-01-29 00:40:58 -0800

    Create Entry struct

---
+++ src/Structs/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DatLibrary.Structs;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x10, Pack = 1)]
+public struct Entry
+{
+	[FieldOffset(0x00)]
+	public uint Offset;
+
+	[FieldOffset(0x04)]
+	public uint Size;
+
+	[FieldOffset(0x08)]
+	public uint UncompressedSize;
+
+	[FieldOffset(0x0C)]
+	public uint CompressFlag;
+}
